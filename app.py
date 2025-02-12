dbae124eb7cf1326eeda6292a3c712662f6f7c1f
JauniKappauni <social.jauni@gmail.com>
2025-02-12 18:33:42 +0100

    Added some imports, which are necessary Added variable secret to create a random number Added variable totp to create a 2fa code based on the secret Added otp_url to identify the code in example google authenticator Added variable qr to create a qrcode based on the otp_url Added variable buffer to have the qr code stored temporarily in memory instead of the hard drive Added saving of qr code to the memory in the .png format Added variable qr_base64 to get the contents of the memory(bytes io storage) as binary value and converted image data to base64 string and finally decoded it to enable using it in html Added GET and POST method to / directive Added empty string value to variable status to prevent "unboundlocalerror" Added if statement to only continue code on button click Added value of template variable from html form to user_input variable Added if statement to determine output of status variable value based on user input Added return of qrcode and status variables

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import qrcode
+import pyotp
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
-@app.route("/")
+secret = pyotp.random_base32()
+totp = pyotp.TOTP(secret)
+otp_url = totp.provisioning_uri(name="test", issuer_name="test")
+
+qr = qrcode.make(otp_url)
+buffer = BytesIO()
+qr.save(buffer, format="PNG")
+qr_base64 = base64.b64encode(buffer.getvalue()).decode()
+
+@app.route("/", methods=["GET", "POST"])
 def function1():
-    return render_template("index.html")
+    status = ""
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        if totp.verify(user_input):
+            status = "✅"
+        else:
+            status = "❌"
+    return render_template("index.html", status=status, qr_base64=qr_base64)
 
 if __name__ == "__main__":
     app.run(debug=True)
